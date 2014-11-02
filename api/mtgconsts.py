8a21586a40fb2bbf72093b92c608d237cb6d99c5
Stephen Biston <stephen@britecore.com>
2014-11-01 22:46:58 -0500

    initial

---
+++ api/mtgconsts.py
@@ -0,0 +1,4 @@
+FIELDS=["manacost", "convertedmanacost", "type",
+        "subtype", "power", "toughness", "loyalty",
+        "rarity", "artist", "setId", "description",
+        "color", "name", "flavor" ]
