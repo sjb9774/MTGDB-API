bbfdb3cd0f94d4bc1a8ad095e138207e8b6c3ff2
Stephen Biston <stephen@britecore.com>
2015-02-28 12:46:19 -0600

    Abstract search criteria out, adjust tests

--- api/complexsearch.py
+++ api/complexsearch.py
@@ -35,26 +35,84 @@ class Search():
         """
         Creates a new Search object.
         """
-        self.query_dict = {}
+        self.search_criteria = SearchCriteria()
+
+        for field in FIELDS:
+            # dynamically creates functions to deal with each field
+            setattr(self, field, self._make_criteria_mod_wrapper(field))
+
+    def _make_criteria_mod_wrapper(self, field):
+        def fn(**kwargs):
+            actual_mod_fn = getattr(self.search_criteria, field)
+            actual_mod_fn(**kwargs)
+            return self
+        return fn
+
+    def process(self):
+        self.search_criteria.process()
+        return self
+
+    def matches(self, dict):
+        """
+        Accepts a dictionary as a container for all the search-criteria arguments. Instead
+        of making chain calls, use this method if you need to aggregate a number of
+        filters and pass them all at once.
+
+        dict =
+        {
+            'mana_cost':
+                {
+                    'equals': 6
+                }
+            'colors':
+                {
+                    'contains': ['red', 'blue']
+                }
+        }
+        """
+        pass
+
+
+    def search(self):
+        """
+        Submits the query to the database and returns the results of the search.
+
+        :returns: A CardList containing the results of the search.
+        """
+        url = "{0}/{1}/{2}".format(MTG_DB_URL, SIMPLE_SEARCH_PATH, self.search_criteria.get_query())
+        results = dbaccess._process_simple_request(url)
+        cards = []
+        for card_data in results:
+            cards.append(Card(card_data))
+        return CardList(cards)
+
+
+    def __str__(self):
+        return str(self.search_criteria)
+
+class SearchCriteria():
+
+    def __init__(self):
+        self.filter_dict = {}
         self.count = False
         self.query = "?q="
 
         for field in FIELDS:
-            self.query_dict[field] = {}
+            self.filter_dict[field] = {}
             for key in kw_map:
-                self.query_dict[field][key] = []
+                self.filter_dict[field][key] = []
 
         for field in FIELDS:
             # dynamically creates functions to deal with each field
-            setattr(self, field, self._make_query_dict_modifier(field))
+            setattr(self, field, self._make_filter_dict_modifier(field))
 
-    def _make_query_dict_modifier(self, field):
+    def _make_filter_dict_modifier(self, field):
 
         def fn(**kwargs):
             for key, value in kwargs.items():
                 if key in kw_map:
-                    if value not in self.query_dict[field][key]:
-                        self.query_dict[field][key].append(value)
+                    if value not in self.filter_dict[field][key]:
+                        self.filter_dict[field][key].append(value)
                 else:
                     raise Exception("{0} is not a valid keyword.".format(key))
             return self
@@ -71,9 +129,9 @@ class Search():
         """
         q = "?q="
         lst = []
-        for field in self.query_dict:
-            for keyword in self.query_dict[field]:
-                for criteria in self.query_dict[field][keyword]:
+        for field in self.filter_dict:
+            for keyword in self.filter_dict[field]:
+                for criteria in self.filter_dict[field][keyword]:
                     lst.append("{0} {1} '{2}'".format(field, kw_map[keyword], criteria))
 
         q += " and ".join(lst)
@@ -84,20 +142,5 @@ class Search():
 
         return self
 
-
-    def search(self):
-        """
-        Submits the query to the database and returns the results of the search.
-
-        :returns: A CardList containing the results of the search.
-        """
-        url = "{0}/{1}/{2}".format(MTG_DB_URL, SIMPLE_SEARCH_PATH, self.query)
-        results = dbaccess._process_simple_request(url)
-        cards = []
-        for card_data in results:
-            cards.append(Card(card_data))
-        return CardList(cards)
-
-
-    def __str__(self):
-        return str(self.query)
+    def get_query(self):
+        return self.query
--- tests/unit_tests/test_complex_search.py
+++ tests/unit_tests/test_complex_search.py
@@ -8,12 +8,12 @@ class TestComplexSearch(unittest.TestCase):
 
     def test_query_holds(self):
         self.search.name(contains='David')
-        self.assertEquals(self.search.query_dict['name']['contains'][0], 'David')
+        self.assertEquals(self.search.search_criteria.filter_dict['name']['contains'][0], 'David')
 
     def test_removal_of_duplicate_search_terms(self):
         s = Search()
         s.name(contains='Thassa').convertedmanacost(less_than=5).type(contains='Enchantment')
-        q = s.process().query
+        q = s.process().search_criteria.query
         s.name(contains='Thassa').type(contains='Enchantment').process()
 
-        self.assertEquals(s.query, q)
+        self.assertEquals(s.search_criteria.query, q)
